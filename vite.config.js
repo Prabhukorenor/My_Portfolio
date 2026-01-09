e8e835c39e67b9f5f52a790589447ed04102affa
Prabhukorenor <prabhukorenor@gmail.com>
2026-01-10 01:21:45 +0530

    fourth commit

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,4 @@
 
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   base: "/my-portfolio/",
-//   plugins: [
-//     react({
-//       babel: {
-//         plugins: [["babel-plugin-react-compiler"]],
-//       },
-//     }),
-//   ],
-// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
