470768e3c89800db7091b450ebb7d7ed4b3b0423
Prabhukorenor <prabhukorenor@gmail.com>
2026-06-15 11:55:32 +0530

    Fix Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   base: "/My_Portfolio/",
+//   plugins: [react()],
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/My_Portfolio/",
+  base: "/",
   plugins: [react()],
 });
